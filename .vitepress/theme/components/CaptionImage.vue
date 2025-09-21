6fb8605f0963993aa51fb5b91a33758f6e90d964
nextrg <oliwiercz2@wp.pl>
2025-09-21 21:16:32 +0200

    add custom components
    caption image & hex color display

---
+++ .vitepress/theme/components/CaptionImage.vue
@@ -0,0 +1,36 @@
+<template>
+  <figure class="caption-image">
+    <img :width="width" :src="src" :alt="alt" />
+    <figcaption style="color: gray; margin-top: 2px">
+      {{ caption }}
+    </figcaption>
+  </figure>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+defineProps({
+  src: String,
+  alt: {
+    type: String,
+    default: "",
+  },
+  caption: {
+    type: String,
+    default: "",
+  },
+  width: {
+    type: String,
+    default: "50%",
+  },
+});
+</script>
+
+<style scoped>
+.caption-image figcaption {
+  color: gray;
+  margin-top: 2px;
+  text-align: center;
+}
+</style>
